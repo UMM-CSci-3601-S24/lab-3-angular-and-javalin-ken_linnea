956826f2b252dab45ff0b4cff88060646c2714be
Ken Broden <brod0268@morris.umn.edu>
2024-02-11 17:04:18 -0600

    formatting updates
    Updated some formatting in the individual cards.  I also ran 'Format Document' on todo.service.ts which moved some things around, which makes it look like more is changing than actually is

--- client/src/app/todos/todo.service.ts
+++ client/src/app/todos/todo.service.ts
@@ -16,19 +16,19 @@ export class TodoService {
   constructor(private httpClient: HttpClient) {
   }
 
-  getTodos(filters?: {category?: string}): Observable<Todo[]> {
-// `HttpParams` is essentially just a map used to hold key-value
+  getTodos(filters?: { category?: string }): Observable<Todo[]> {
+    // `HttpParams` is essentially just a map used to hold key-value
     // pairs that are then encoded as "?key1=value1&key2=value2&…" in
     // the URL when we make the call to `.get()` below.
     let httpParams: HttpParams = new HttpParams();
     if (filters) {
-      if(filters.category) {
+      if (filters.category) {
         httpParams = httpParams.set('category', filters.category)
       }
     }
-// Send the HTTP GET request with the given URL and parameters.
+    // Send the HTTP GET request with the given URL and parameters.
     // That will return the desired `Observable<User[]>`
-    return this.httpClient.get<Todo[]>(this.todoUrl, {params: httpParams},);
+    return this.httpClient.get<Todo[]>(this.todoUrl, { params: httpParams },);
   }
 
   /**
@@ -40,38 +40,38 @@ export class TodoService {
   getTodoById(id: string): Observable<Todo> {
     return this.httpClient.get<Todo>(this.todoUrl + '/' + id);
   }
-      /**
-   * A service method that filters an array of `User` using
-   * the specified filters.
-   *
-   * Note that the filters here support partial matches. Since the
-   * matching is done locally we can afford to repeatedly look for
-   * partial matches instead of waiting until we have a full string
-   * to match against.
-   *
-   * @param users the array of `Users` that we're filtering
-   * @param filters the map of key-value pairs used for the filtering
-   * @returns an array of `Users` matching the given filters
-   */
+  /**
+* A service method that filters an array of `User` using
+* the specified filters.
+*
+* Note that the filters here support partial matches. Since the
+* matching is done locally we can afford to repeatedly look for
+* partial matches instead of waiting until we have a full string
+* to match against.
+*
+* @param users the array of `Users` that we're filtering
+* @param filters the map of key-value pairs used for the filtering
+* @returns an array of `Users` matching the given filters
+*/
 
-  filterTodos(todos: Todo[], filters: { body?: string, owner?: string, status?: boolean, limit?: number}): Todo[] {
-let filteredTodos = todos;
+  filterTodos(todos: Todo[], filters: { body?: string, owner?: string, status?: boolean, limit?: number }): Todo[] {
+    let filteredTodos = todos;
 
-if (filters.body) {
-  filters.body = filters.body.toLowerCase();
-  filteredTodos = filteredTodos.filter(todo => todo.body.toLowerCase().indexOf(filters.body) !== -1);
-}
-if (filters.owner) {
-  filters.owner = filters.owner.toLowerCase();
-  filteredTodos = filteredTodos.filter(todo => todo.owner.toLowerCase().indexOf(filters.owner) !== -1);
-}
-if (filters.status !== undefined) {
-  filteredTodos = filteredTodos.filter(todo => todo.status === filters.status);
-}
-if (filters.limit) {
-  filteredTodos = filteredTodos.slice(0, filters.limit);
-}
-return filteredTodos;
+    if (filters.body) {
+      filters.body = filters.body.toLowerCase();
+      filteredTodos = filteredTodos.filter(todo => todo.body.toLowerCase().indexOf(filters.body) !== -1);
+    }
+    if (filters.owner) {
+      filters.owner = filters.owner.toLowerCase();
+      filteredTodos = filteredTodos.filter(todo => todo.owner.toLowerCase().indexOf(filters.owner) !== -1);
+    }
+    if (filters.status !== undefined) {
+      filteredTodos = filteredTodos.filter(todo => todo.status === filters.status);
+    }
+    if (filters.limit) {
+      filteredTodos = filteredTodos.slice(0, filters.limit);
+    }
+    return filteredTodos;
   }
 }
 
